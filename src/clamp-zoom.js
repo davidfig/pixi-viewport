fbd073f1656182b9632659ad164af48e5c43c9f4
Samson <qwertysam@users.noreply.github.com>
2017-10-18 16:00:35 -0400

    Updates clamp() upon resizing of the viewport
    This is important so that if the window size is changed, the viewport maintains its contraints.
    e.g. If the viewport is at its max constraints and the window size increases, the viewport should clamp to those max contraints rather than allow the viewport to show more of the world.

--- src/clamp-zoom.js
+++ src/clamp-zoom.js
@@ -18,6 +18,11 @@ module.exports = class ClampZoon extends Plugin
         this.maxHeight = options.maxHeight
     }
 
+    resize()
+    {
+        clamp()
+    }
+    
     clamp()
     {
         let width = this.parent.worldScreenWidth
