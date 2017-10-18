859f4de77df0b229098a4d5e207f8cc71ad1aed3
Samson <qwertysam@users.noreply.github.com>
2017-10-18 17:14:11 -0400

    Updated documentation
    NOTE: Must still be updated in README.md

--- src/viewport.js
+++ src/viewport.js
@@ -573,6 +573,7 @@ module.exports = class Viewport extends Loop
     /**
      * add a hitArea to the container -- useful when your container contains empty spaces that you'd like to drag or pinch
      * @param {PIXI.Rectangle} [rect] if no rect is provided, it will use the value of container.getBounds()
+     * @return {Viewport} this
      */
     hitArea(rect)
     {
@@ -586,6 +587,7 @@ module.exports = class Viewport extends Loop
      * @param {number} y
      * @param {object} [options]
      * @param {number} [options.speed=1] speed (in world pixels/ms) to snap to location
+     * @return {Viewport} this
      */
     snap(x, y, options)
     {
@@ -599,6 +601,7 @@ module.exports = class Viewport extends Loop
      * @param {object} [options]
      * @param {number} [options.speed=0] to follow in pixels/frame
      * @param {number} [options.radius] radius (in world coordinates) of center circle where movement is allowed without moving the viewport
+     * @return {Viewport} this
      */
     follow(target, options)
     {
@@ -616,6 +619,7 @@ module.exports = class Viewport extends Loop
      * @param {number} [options.minHeight] clamp minimum height
      * @param {number} [options.maxWidth] clamp maximum width
      * @param {number} [options.maxHeight] clamp maximum height
+     * @return {Viewport} this
      */
     wheel(options)
     {
@@ -623,6 +627,16 @@ module.exports = class Viewport extends Loop
         return this
     }
 
+    /**
+     * enable clamping of zoom to constraints
+     * NOTE: screenWidth, screenHeight, worldWidth, and worldHeight needs to be set for this to work properly
+     * @param {object} [options]
+     * @param {number} [options.minWidth] minimum width
+     * @param {number} [options.minHeight] minimum height
+     * @param {number} [options.maxWidth] maximum width
+     * @param {number} [options.maxHeight] maximum height
+     * @return {Viewport} this
+     */
     clampZoom(options)
     {
         this.plugins['clamp-zoom'] = new ClampZoom(this, options)
