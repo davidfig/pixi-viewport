b39294be9c83e2b16d363df3cb12f9cda3024fe8
Samson <stanmoon95@gmail.com>
2017-10-27 16:08:11 -0400

    Fixed bug

--- src/snap.js
+++ src/snap.js
@@ -94,7 +94,7 @@ module.exports = class Snap extends Plugin
             {
                 this.parent.removePlugin('snap')
             }
-            this.parent.emit('snap-end', { screen: this.parent.toScreen({this.x, this.y}), world: {this.x, this.y}, viewport: this.parent })
+            this.parent.emit('snap-end', { screen: this.parent.toScreen({{ x: this.x, y: this.y }}), world: {{ x: this.x, y: this.y }}, viewport: this.parent })
             this.snapping = null
         }
     }
