f6fd6b826e2909b57dea831c01964a2e6661e9f9
David Figatner <david@yopeyopey.com>
2017-11-03 13:11:18 +0800

    cleaning up

--- src/snap.js
+++ src/snap.js
@@ -9,14 +9,15 @@ module.exports = class Snap extends Plugin
      * @param {number} x
      * @param {number} y
      * @param {object} [options]
-     * @param {boolean} [options.center] snap to the center of the camera instead of the top-left corner of viewport
+     * @param {boolean} [options.topLeft] snap to the top-left of viewport instead of center
      * @param {number} [options.friction=0.8] friction/frame to apply if decelerate is active
      * @param {number} [options.time=1000]
      * @param {string|function} [options.ease=easeInOutSine] ease function or name (see http://easings.net/ for supported names)
      * @param {boolean} [options.interrupt=true] pause snapping with any user input on the viewport
-     * @param {boolean} [options.removeOnComplete=true] removes this plugin after snapping is complete
+     * @param {boolean} [options.removeOnComplete] removes this plugin after snapping is complete
      *
      * @event snap-start(Viewport) emitted each time a snap animation starts
+     * @event snap-restart(Viewport) emitted each time a snap resets because of a change in viewport size
      * @event snap-end(Viewport) emitted each time snap reaches its target
      */
     constructor(parent, x, y, options)
@@ -26,20 +27,13 @@ module.exports = class Snap extends Plugin
         this.friction = options.friction || 0.8
         this.time = options.time || 1000
         this.ease = options.ease || 'easeInOutSine'
-        this.targetX = x
-        this.targetY = y
-        this.center = options.center
-        if (!this.center)
-        {
-            // Always target the center, and calculate the new top-left corner to target upon resizing
-            this.originalTargetX = this.targetX
-            this.originalTargetY = this.targetY
-            
-            this.resize()
-        }
+        this.x = x
+        this.y = y
+        this.topLeft = options.topLeft
         this.originalCenter = this.parent.center
         this.interrupt = exists(options.interrupt) ? options.interrupt : true
-        this.removeOnComplete = exists(options.removeOnComplete) ? options.removeOnComplete: true
+        this.removeOnComplete = options.removeOnComplete
+        this.calculateTarget()
     }
 
     reset()
@@ -49,7 +43,13 @@ module.exports = class Snap extends Plugin
 
     resize()
     {
-        if (!this.center)
+        this.calculateTarget()
+    }
+
+
+    calculateTarget()
+    {
+        if (!this.topLeft)
         {
             /* Finds target center based on the given originalTarget point (the top left corner)
              * DOES NOT WORK WHEN the snap-zoom plugin is working simultaneously because this.parent.container.scale.x and y are
@@ -61,7 +61,7 @@ module.exports = class Snap extends Plugin
             this.targetY = this.parent.worldScreenHeight / 2 + this.originalTargetY
         }
     }
-    
+
     down()
     {
         this.snapping = null
@@ -97,10 +97,15 @@ module.exports = class Snap extends Plugin
         }
         else if (this.snapping)
         {
-            var finished = this.snapping.update(elapsed)
-            
+            if (this.parent.scale.x !== this.lastScaleX || this.parent.scale.y !== this.lastScaleY)
+            {
+                this.snapping = new Ease.to(this.originalCenter, { x: this.targetX, y: this.targetY }, this.time, { ease: this.eaes })
+                this.parent.emit('snap-restart', this.parent)
+            }
+            const finished = this.snapping.update(elapsed)
+
             this.parent.moveCenter(this.originalCenter)
-            
+
             if (finished)
             {
                 if (this.removeOnComplete)
