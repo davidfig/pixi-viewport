eaab00f489915a9a1413c72411b3d9f2797b696d
David Figatner <david@yopeyopey.com>
2019-07-07 18:55:00 -0400

    fixed dependency

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import builtins from 'rollup-plugin-node-builtins'
+import builtins from '@joseph184/rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 
 export default {
--- rollup.dev.js
+++ rollup.dev.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import serve from 'rollup-plugin-serve'
-import builtins from 'rollup-plugin-node-builtins'
+import builtins from '@joseph184/rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 
 export default
