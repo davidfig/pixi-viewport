cb06faff550377c48104d210e20b0f7b52719b80
Shukant Pal <sukantk2002@outlook.com>
2021-04-11 14:26:41 -0400

    No more /compiel

--- compile/ease.d.ts
+++
@@ -1,1 +0,0 @@
-export default function ease(ease: any, defaults?: any): any;
--- compile/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './plugins';
-export * from './ease';
-export * from './InputManager';
-export * from './PluginManager';
-export * from './Viewport';
