7a31de4dfdc3b147904d5d5a4a559478ae01b59c
Shukant Pal <sukantk2002@outlook.com>
2021-04-11 13:40:55 -0400

    New build

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,5 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import { terser } from 'rollup-plugin-terser'
-import { babel } from '@rollup/plugin-babel'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import { main } from '@pixi-build-tools/rollup-configurator/main';
 
-export default [
-    {
-        input: 'src/Viewport.ts',
-        plugins: [
-            peerDepsExternal(),
-            resolve(
-                {
-                    preferBuiltins: false
-                }),
-            commonjs(),
-            terser(),
-            babel({
-                babelHelpers: 'bundled',
-                presets: ['@babel/preset-env']
-            }),
-        ],
-        output:
-        {
-            file: 'dist/Viewport.ts',
-            globals:
-            {
-                'pixi.js': 'PIXI'
-            },
-            format: 'umd',
-            name: 'Viewport',
-            sourcemap: true
-        }
-    },
-    {
-        input: 'src/Viewport.ts',
-        plugins: [
-            peerDepsExternal(),
-            resolve(
-                {
-                    preferBuiltins: false
-                }),
-            commonjs()
-        ],
-        output:
-        {
-            file: 'dist/viewport.es.js',
-            format: 'esm',
-            sourcemap: true
-        }
-    }]
+export default main({
+   excludeExternals: ['penner'],
+});
