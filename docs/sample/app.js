ce36cb2f73a208d904769c86ef429279f548efdc
David Figatner <david@yopeyopey.com>
2021-04-21 07:04:09 +0800

    continuing to rework sample code

---
+++ docs/sample/app.js
@@ -0,0 +1,44 @@
+// import * as PIXI from 'pixi.js'  // use with modern build toolchain
+import * as viewport from './viewport'
+import * as target from './target'
+
+let renderer
+
+function createRenderer() {
+    renderer = new PIXI.Renderer({
+        backgroundAlpha: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        resolution: window.devicePixelRatio,
+        antialias: true,
+    })
+    document.body.appendChild(renderer.view)
+    renderer.view.style.position = 'fixed'
+    renderer.view.style.width = '100vw'
+    renderer.view.style.height = '100vh'
+    renderer.view.style.top = 0
+    renderer.view.style.left = 0
+    renderer.view.style.background = 'rgba(0,0,0,.1)'
+}
+
+function start() {
+    createRenderer()
+    viewport.create()
+    window.onresize = () => {
+        renderer.resize(window.innerWidth, window.innerHeight)
+        viewport.get().resize(window.innerWidth, window.innerHeight)
+    }
+    update()
+}
+
+function update() {
+    const vp = viewport.get()
+    if (vp.dirty || target.isDirty()) {
+        target.update()
+        renderer.render(vp)
+        vp.dirty = false
+    }
+    requestAnimationFrame(() => update())
+}
+
+window.onload = start
--- docs/sample/border.js
+++
@@ -1,4 +0,0 @@
-export function border(viewport) {
-    const line = viewport.addChild(new PIXI.Graphics())
-    line.lineStyle(10, 0xff0000).drawRect(0, 0, viewport.worldWidth, viewport.worldHeight)
-}
