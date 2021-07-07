bd5559c30670d7ff75524aca9a025689f9a09694
sascha <sascha@ehle.rs>
2021-07-07 13:53:04 +0200

    feat: add keyToPress option to the wheel-plugin like done in the drag-plugin

--- src/plugins/Wheel.ts
+++ src/plugins/Wheel.ts
@@ -6,54 +6,62 @@ import type { Viewport } from '../Viewport';
 /** Options for {@link Wheel}. */
 export interface IWheelOptions
 {
-    /**
-     * Percent to scroll with each spin
-     *
-     * @default 0.1
-     */
-    percent?: number;
+  /**
+   * Percent to scroll with each spin
+   *
+   * @default 0.1
+   */
+  percent?: number;
 
-    /**
-     * smooth the zooming by providing the number of frames to zoom between wheel spins
-     *
-     * @default false
-     */
-    smooth?: false | number;
+  /**
+   * smooth the zooming by providing the number of frames to zoom between wheel spins
+   *
+   * @default false
+   */
+  smooth?: false | number;
 
-    /**
-     * Stop smoothing with any user input on the viewport
-     *
-     * @default true
-     */
-    interrupt?: boolean;
+  /**
+   * Stop smoothing with any user input on the viewport
+   *
+   * @default true
+   */
+  interrupt?: boolean;
 
-    /**
-     * Reverse the direction of the scroll
-     *
-     * @default false
-     */
-    reverse?: boolean;
+  /**
+   * Reverse the direction of the scroll
+   *
+   * @default false
+   */
+  reverse?: boolean;
 
-    /**
-     * Place this point at center during zoom instead of current mouse position
-     *
-     * @default null
-     */
-    center?: Point | null;
+  /**
+   * Place this point at center during zoom instead of current mouse position
+   *
+   * @default null
+   */
+  center?: Point | null;
 
-    /**
-     * Scaling factor for non-DOM_DELTA_PIXEL scrolling events
-     *
-     * @default 20
-     */
-    lineHeight?: number;
+  /**
+   * Scaling factor for non-DOM_DELTA_PIXEL scrolling events
+   *
+   * @default 20
+   */
+  lineHeight?: number;
 
-    /**
-     * Axis to zoom
-     *
-     * @default 'all'
-     */
-    axis?: 'all' | 'x' | 'y';
+  /**
+   * Axis to zoom
+   *
+   * @default 'all'
+   */
+  axis?: 'all' | 'x' | 'y';
+
+  /**
+   * Array containing {@link key|https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code} codes of
+   * keys that can be pressed for the zoom to be triggered, e.g.: ['ShiftLeft', 'ShiftRight'}.
+   *
+   * @default null
+   */
+  keyToPress?: string[] | null;
 }
 
 const DEFAULT_WHEEL_OPTIONS: Required<IWheelOptions> = {
@@ -64,6 +72,7 @@ const DEFAULT_WHEEL_OPTIONS: Required<IWheelOptions> = {
     center: null,
     lineHeight: 20,
     axis: 'all',
+    keyToPress: null,
 };
 
 /**
@@ -73,159 +82,202 @@ const DEFAULT_WHEEL_OPTIONS: Required<IWheelOptions> = {
  */
 export class Wheel extends Plugin
 {
-    public readonly options: Required<IWheelOptions>;
+  public readonly options: Required<IWheelOptions>;
 
-    protected smoothing?: IPointData | null;
-    protected smoothingCenter?: Point | null;
-    protected smoothingCount?: number;
+  protected smoothing?: IPointData | null;
+  protected smoothingCenter?: Point | null;
+  protected smoothingCount?: number;
 
-    /**
-     * This is called by {@link Viewport.wheel}.
-     */
-    constructor(parent: Viewport, options: IWheelOptions = {})
-    {
-        super(parent);
-        this.options = Object.assign({}, DEFAULT_WHEEL_OPTIONS, options);
-    }
+  /** Flags whether the keys required to zoom are pressed currently. */
+  protected keyIsPressed: boolean;
 
-    public down(): boolean
-    {
-        if (this.options.interrupt)
-        {
-            this.smoothing = null;
-        }
+  /**
+   * This is called by {@link Viewport.wheel}.
+   */
+  constructor(parent: Viewport, options: IWheelOptions = {})
+  {
+      super(parent);
+      this.options = Object.assign({}, DEFAULT_WHEEL_OPTIONS, options);
+      this.keyIsPressed = false;
 
-        return false;
-    }
+      if (this.options.keyToPress)
+      {
+          this.handleKeyPresses(this.options.keyToPress);
+      }
+  }
 
-    protected isAxisX(): boolean
-    {
-        return ['all', 'x'].includes(this.options.axis);
-    }
+  /**
+   * Handles keypress events and set the keyIsPressed boolean accordingly
+   *
+   * @param {array} codes - key codes that can be used to trigger zoom event
+   */
+  protected handleKeyPresses(codes: string[]): void
+  {
+      window.addEventListener('keydown', (e) =>
+      {
+          if (codes.includes(e.code))
+          {
+              this.keyIsPressed = true;
+          }
+      });
 
-    protected isAxisY(): boolean
-    {
-        return ['all', 'y'].includes(this.options.axis);
-    }
+      window.addEventListener('keyup', (e) =>
+      {
+          if (codes.includes(e.code))
+          {
+              this.keyIsPressed = false;
+          }
+      });
+  }
 
-    public update(): void
-    {
-        if (this.smoothing)
-        {
-            const point = this.smoothingCenter;
-            const change = this.smoothing;
-            let oldPoint;
+  protected checkKeyPress(): boolean
+  {
+      return !this.options.keyToPress || this.keyIsPressed;
+  }
 
-            if (!this.options.center)
-            {
-                oldPoint = this.parent.toLocal(point as IPointData);
-            }
-            if (this.isAxisX())
-            {
-                this.parent.scale.x += change.x;
-            }
-            if (this.isAxisY())
-            {
-                this.parent.scale.y += change.y;
-            }
+  public down(): boolean
+  {
+      if (this.options.interrupt)
+      {
+          this.smoothing = null;
+      }
 
-            this.parent.emit('zoomed', { viewport: this.parent, type: 'wheel' });
-            const clamp = this.parent.plugins.get('clamp-zoom', true);
+      return false;
+  }
 
-            if (clamp)
-            {
-                clamp.clamp();
-            }
-            if (this.options.center)
-            {
-                this.parent.moveCenter(this.options.center);
-            }
-            else
-            {
-                const newPoint = this.parent.toGlobal(oldPoint as IPointData);
+  protected isAxisX(): boolean
+  {
+      return ['all', 'x'].includes(this.options.axis);
+  }
 
-                this.parent.x += (point as IPointData).x - newPoint.x;
-                this.parent.y += (point as IPointData).y - newPoint.y;
-            }
+  protected isAxisY(): boolean
+  {
+      return ['all', 'y'].includes(this.options.axis);
+  }
 
-            this.parent.emit('moved', { viewport: this.parent, type: 'wheel' });
-            (this.smoothingCount as number)++;
+  public update(): void
+  {
+      if (this.smoothing)
+      {
+          const point = this.smoothingCenter;
+          const change = this.smoothing;
+          let oldPoint;
 
-            if (this.smoothingCount as number >= this.options.smooth)
-            {
-                this.smoothing = null;
-            }
-        }
-    }
+          if (!this.options.center)
+          {
+              oldPoint = this.parent.toLocal(point as IPointData);
+          }
+          if (this.isAxisX())
+          {
+              this.parent.scale.x += change.x;
+          }
+          if (this.isAxisY())
+          {
+              this.parent.scale.y += change.y;
+          }
 
-    public wheel(e: WheelEvent): boolean | undefined
-    {
-        if (this.paused)
-        {
-            return;
-        }
+          this.parent.emit('zoomed', { viewport: this.parent, type: 'wheel' });
+          const clamp = this.parent.plugins.get('clamp-zoom', true);
 
-        const point = this.parent.input.getPointerPosition(e);
-        const sign = this.options.reverse ? -1 : 1;
-        const step = sign * -e.deltaY * (e.deltaMode ? this.options.lineHeight : 1) / 500;
-        const change = Math.pow(2, (1 + this.options.percent) * step);
+          if (clamp)
+          {
+              clamp.clamp();
+          }
+          if (this.options.center)
+          {
+              this.parent.moveCenter(this.options.center);
+          }
+          else
+          {
+              const newPoint = this.parent.toGlobal(oldPoint as IPointData);
 
-        if (this.options.smooth)
-        {
-            const original = {
-                x: this.smoothing ? this.smoothing.x * (this.options.smooth - (this.smoothingCount as number)) : 0,
-                y: this.smoothing ? this.smoothing.y * (this.options.smooth - (this.smoothingCount as number)) : 0
-            };
+              this.parent.x += (point as IPointData).x - newPoint.x;
+              this.parent.y += (point as IPointData).y - newPoint.y;
+          }
 
-            this.smoothing = {
-                x: ((this.parent.scale.x + original.x) * change - this.parent.scale.x) / this.options.smooth,
-                y: ((this.parent.scale.y + original.y) * change - this.parent.scale.y) / this.options.smooth,
-            };
-            this.smoothingCount = 0;
-            this.smoothingCenter = point;
-        }
-        else
-        {
-            let oldPoint: IPointData | undefined;
+          this.parent.emit('moved', { viewport: this.parent, type: 'wheel' });
+          (this.smoothingCount as number)++;
 
-            if (!this.options.center)
-            {
-                oldPoint = this.parent.toLocal(point);
-            }
-            if (this.isAxisX())
-            {
-                this.parent.scale.x *= change;
-            }
-            if (this.isAxisY())
-            {
-                this.parent.scale.y *= change;
-            }
-            this.parent.emit('zoomed', { viewport: this.parent, type: 'wheel' });
-            const clamp = this.parent.plugins.get('clamp-zoom', true);
+          if ((this.smoothingCount as number) >= this.options.smooth)
+          {
+              this.smoothing = null;
+          }
+      }
+  }
 
-            if (clamp)
-            {
-                clamp.clamp();
-            }
-            if (this.options.center)
-            {
-                this.parent.moveCenter(this.options.center);
-            }
-            else
-            {
-                const newPoint = this.parent.toGlobal(oldPoint as IPointData);
+  public wheel(e: WheelEvent): boolean | undefined
+  {
+      if (this.paused)
+      {
+          return;
+      }
 
-                this.parent.x += point.x - newPoint.x;
-                this.parent.y += point.y - newPoint.y;
-            }
-        }
+      if (!this.checkKeyPress())
+      {
+          return;
+      }
 
-        this.parent.emit('moved', { viewport: this.parent, type: 'wheel' });
-        this.parent.emit('wheel', { wheel: { dx: e.deltaX, dy: e.deltaY, dz: e.deltaZ }, event: e, viewport: this.parent });
+      const point = this.parent.input.getPointerPosition(e);
+      const sign = this.options.reverse ? -1 : 1;
+      const step = sign * -e.deltaY * (e.deltaMode ? this.options.lineHeight : 1) / 500;
+      const change = Math.pow(2, (1 + this.options.percent) * step);
 
-        if (!this.parent.options.passiveWheel)
-        {
-            return true;
-        }
-    }
+      if (this.options.smooth)
+      {
+          const original = {
+              x: this.smoothing ? this.smoothing.x * (this.options.smooth - (this.smoothingCount as number)) : 0,
+              y: this.smoothing ? this.smoothing.y * (this.options.smooth - (this.smoothingCount as number)) : 0
+          };
+
+          this.smoothing = {
+              x: ((this.parent.scale.x + original.x) * change - this.parent.scale.x) / this.options.smooth,
+              y: ((this.parent.scale.y + original.y) * change - this.parent.scale.y) / this.options.smooth,
+          };
+          this.smoothingCount = 0;
+          this.smoothingCenter = point;
+      }
+      else
+      {
+          let oldPoint: IPointData | undefined;
+
+          if (!this.options.center)
+          {
+              oldPoint = this.parent.toLocal(point);
+          }
+          if (this.isAxisX())
+          {
+              this.parent.scale.x *= change;
+          }
+          if (this.isAxisY())
+          {
+              this.parent.scale.y *= change;
+          }
+          this.parent.emit('zoomed', { viewport: this.parent, type: 'wheel' });
+          const clamp = this.parent.plugins.get('clamp-zoom', true);
+
+          if (clamp)
+          {
+              clamp.clamp();
+          }
+          if (this.options.center)
+          {
+              this.parent.moveCenter(this.options.center);
+          }
+          else
+          {
+              const newPoint = this.parent.toGlobal(oldPoint as IPointData);
+
+              this.parent.x += point.x - newPoint.x;
+              this.parent.y += point.y - newPoint.y;
+          }
+      }
+
+      this.parent.emit('moved', { viewport: this.parent, type: 'wheel' });
+      this.parent.emit('wheel', { wheel: { dx: e.deltaX, dy: e.deltaY, dz: e.deltaZ }, event: e, viewport: this.parent });
+
+      if (!this.parent.options.passiveWheel)
+      {
+          return true;
+      }
+  }
 }
