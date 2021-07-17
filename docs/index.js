2f7ee521ba22803427cc057c4fa9737105772576
David Figatner <david@yopeyopey.com>
2021-07-17 07:37:25 -0700

    improve demo

--- docs/index.js
+++ docs/index.js
@@ -112,7 +112,7 @@
             worldWidth: WORLD_WIDTH,                        // world width used by viewport (automatically calculated based on container width)
             worldHeight: WORLD_HEIGHT,                      // world height used by viewport (automatically calculated based on container height)
             // threshold: 5,                                // number of pixels to move to trigger an input event (e.g., drag, pinch) or disable a clicked event
-            // passiveWheel: true,                          // whether the 'wheel' event is set to passive (note: if false, e.preventDefault() will be called when wheel is used over the viewport)
+            passiveWheel: false,                            // whether the 'wheel' event is set to passive (note: if false, e.preventDefault() will be called when wheel is used over the viewport)
             // stopPropagation: false,                      // whether to stopPropagation of events that impact the viewport (except wheel events, see options.passiveWheel)
             // forceHitArea: null,                          // change the default hitArea from world size to a new value
             // noTicker: false,                             // set this if you want to manually call update() function on each frame
