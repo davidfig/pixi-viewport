90bfdbd2101a1054171294dc9b17e2e9c9c3525e
cuire <garwes@icloud.com>
2023-01-09 19:29:34 +0500

    globals to umd

--- vite.build.ts
+++ vite.build.ts
@@ -3,22 +3,29 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
+const globals = {
+    '@pixi/core': 'PIXI',
+    '@pixi/display': 'PIXI',
+    '@pixi/events': 'PIXI',
+    '@pixi/math': 'PIXI',
+    '@pixi/ticker': 'PIXI',
+};
+
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'pixi-viewport',
-            fileName: 'pixi-viewport',
+            name: 'pixi_viewport',
+            fileName: 'pixi_viewport',
         },
         rollupOptions: {
             external: [
                 'pixi.js',
-                '@pixi/core',
-                '@pixi/display',
-                '@pixi/events',
-                '@pixi/math',
-                '@pixi/ticker'
+                ...Object.keys(globals),
             ],
+            output: {
+                globals
+            },
         },
     },
     plugins: [dts()]
